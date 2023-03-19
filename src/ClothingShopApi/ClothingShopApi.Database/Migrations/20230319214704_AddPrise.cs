9eb5e6d920659394c410b9646ae65c98c7f1f061
andrew-nasevich <nasevichandrey@gmail.com>
2023-03-19 22:49:04 +0100

    Add DB migrations

---
+++ src/ClothingShopApi/ClothingShopApi.Database/Migrations/20230319214704_AddPrise.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ClothingShopApi.Database.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPrise : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "Price",
+                table: "Clothings",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Price",
+                table: "Clothings");
+        }
+    }
+}
