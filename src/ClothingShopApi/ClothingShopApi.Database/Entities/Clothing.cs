3f6cc87f148e02cb48137a23a7c41ade3261a6f3
andrew-nasevich <nasevichandrey@gmail.com>
2023-03-19 22:48:14 +0100

    Add a "Price" field to the Clothing model

--- src/ClothingShopApi/ClothingShopApi.Database/Entities/Clothing.cs
+++ src/ClothingShopApi/ClothingShopApi.Database/Entities/Clothing.cs
@@ -8,6 +8,7 @@ namespace ClothingShopApi.Database.Entities
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public decimal Price { get; set; }
         public int CategoryId { get; set; }
         public int BrandId { get; set; }
 
